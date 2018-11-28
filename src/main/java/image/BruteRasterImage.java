335e62e5c4307ddbbb214e48bf328d16b8afa86b
RomeoC13 <romeo.chatel@gmail.com>
2018-11-28 10:39:08 +0100

    Création des méthodes getWidth, getHeight, setWidth, setHeight et des constructeurs de la classe BruteRasterImage

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -2,25 +2,37 @@ package image;
 
 import javafx.scene.paint.Color;
 
+import java.util.Arrays;
+
 public class BruteRasterImage implements Image {
 
     public Color[][] pixels;
 
     @Override
     public int getWidth() {
-        return 0;
+        return pixels.length;
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return pixels[0].length;
     }
 
     public BruteRasterImage(Color color, int width, int height){
+        this.pixels=new Color[width][height];
 
+        for(int largeur=0;largeur<width;largeur++){
+
+            for(int longeur=0;longeur<height;longeur++){
+
+                this.pixels[longeur][largeur]=color;
+            }
+        }
     }
 
-    public BruteRasterImage(Color[][] colors){}
+    public BruteRasterImage(Color[][] colors){
+        this.pixels=colors;
+    }
 
     public void createRepresentation(){
 
@@ -43,10 +55,13 @@ public class BruteRasterImage implements Image {
     }
 
     protected void setWidth(int width){
-
+        pixels=Arrays.copyOf(pixels,width);
     }
 
     protected void setHeight(int height){
+        for(int width=0;width<getHeight();width++) {
+            pixels[width] = Arrays.copyOf(pixels[width], height);
+        }
 
     }
 }
