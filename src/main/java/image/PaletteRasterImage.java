78f3e471df069a526e27cc07b5e4312bbdbb478d
LindermanLD <gaminglindermanld@gmail.com>
2018-11-28 11:57:47 +0100

    Réimplementation à cause d'une erreur de merge

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -2,6 +2,7 @@ package image;
 
 import javafx.scene.paint.Color;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PaletteRasterImage implements Image{
@@ -48,21 +49,33 @@ public class PaletteRasterImage implements Image{
 
 
     public void createRepresentation(){
-
+        this.palette = new ArrayList<Color>();
+        this.indexesOfColors = new int[this.width][this.height];
     }
 
     public PaletteRasterImage(Color[][] pixels){
-
+        setHeight(pixels[0].length);
+        setWidth(pixels.length);
+        createRepresentation();
+        setPixelsColor(pixels);
     }
 
     public PaletteRasterImage(Color color, int width, int height){
-
+        setHeight(height);
+        setWidth(width);
+        createRepresentation();
+        setPixelsColor(color);
     }
 
     @Override
     public Color getPixelColor(int x, int y) {
         return palette.get(this.indexesOfColors[x][y]);
     }
+    protected void setWidth(int width){
+        this.width=width;
+    }
 
-
+    protected void setHeight(int height){
+        this.height=height;
+    }
 }
